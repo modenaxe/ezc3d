234bd8c7a26ed9753ae035efebe674afa578e97e
Luca Modenese <modenaxe@users.noreply.github.com>
2026-02-04 17:24:23 +1100

    Fix WASM bindings for Vector3d and ForcePlatforms
    Updated bindings to reflect correct types and method names.

--- src/wasm_bindings.cpp
+++ src/wasm_bindings.cpp
@@ -5,6 +5,7 @@ using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(ezc3d_wasm) {
     // --- Math Bindings ---
+    // Note: Vector3d uses double, not float
     class_<ezc3d::Vector3d>("Vector3d")
         .function("x", select_overload<double() const>(&ezc3d::Vector3d::x))
         .function("y", select_overload<double() const>(&ezc3d::Vector3d::y))
@@ -30,23 +31,24 @@ EMSCRIPTEN_BINDINGS(ezc3d_wasm) {
 
     class_<ezc3d::Modules::ForcePlatforms>("ForcePlatforms")
         .constructor<const ezc3d::c3d&>()
-        // Corrected method name from nbPlatforms to nbForcePlatforms
-        .function("nbPlatforms", &ezc3d::Modules::ForcePlatforms::nbForcePlatforms)
+        // FIXED: 'nbForcePlatforms' does not exist. Bind the vector accessor instead.
+        .function("forcePlatforms", &ezc3d::Modules::ForcePlatforms::forcePlatforms)
         .function("forcePlatform", &ezc3d::Modules::ForcePlatforms::forcePlatform);
 
-    // --- Header Bindings (Resolved Overloads) ---
+    // --- Header Bindings ---
     class_<ezc3d::Header>("Header")
         .function("nbFrames", &ezc3d::Header::nbFrames)
         .function("frameRate", select_overload<float() const>(&ezc3d::Header::frameRate))
-        // Explicitly cast overloaded size getters
         .function("nb3dPoints", select_overload<size_t() const>(&ezc3d::Header::nb3dPoints))
         .function("nbAnalogs", select_overload<size_t() const>(&ezc3d::Header::nbAnalogs));
 
-    // --- Data Point Bindings (Resolved Overloads) ---
+    // --- Data Point Bindings ---
+    // FIXED: Point::x/y/z return double, not float
     class_<ezc3d::DataNS::Points3dNS::Point>("Point")
-        .function("x", select_overload<float() const>(&ezc3d::DataNS::Points3dNS::Point::x))
-        .function("y", select_overload<float() const>(&ezc3d::DataNS::Points3dNS::Point::y))
-        .function("z", select_overload<float() const>(&ezc3d::DataNS::Points3dNS::Point::z));
+        .function("x", select_overload<double() const>(&ezc3d::DataNS::Points3dNS::Point::x))
+        .function("y", select_overload<double() const>(&ezc3d::DataNS::Points3dNS::Point::y))
+        .function("z", select_overload<double() const>(&ezc3d::DataNS::Points3dNS::Point::z))
+        .function("residual", select_overload<double() const>(&ezc3d::DataNS::Points3dNS::Point::residual));
 
     // --- Main C3D Binding ---
     class_<ezc3d::c3d>("c3d")
