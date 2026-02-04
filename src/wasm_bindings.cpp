32ce3bd6ceb63c0bedc1cdff0d1f5d63708c25e9
Luca Modenese <modenaxe@users.noreply.github.com>
2026-02-05 03:21:03 +1100

    Enhance Matrix and Vector3d bindings with constructors
    Added constructors for Matrix and Matrix44 classes, and updated Vector3d bindings to include Matrix inheritance. Introduced ForcePlatform bindings for additional functionality.

--- src/wasm_bindings.cpp
+++ src/wasm_bindings.cpp
@@ -8,6 +8,7 @@ EMSCRIPTEN_BINDINGS(ezc3d_wasm) {
     class_<ezc3d::Matrix>("Matrix")
         .constructor<>()
         .constructor<size_t, size_t>()
+        .constructor<const ezc3d::Matrix&>()
         .function("print", &ezc3d::Matrix::print)
         .function("nbRows", &ezc3d::Matrix::nbRows)
         .function("nbCols", &ezc3d::Matrix::nbCols)
@@ -16,12 +17,12 @@ EMSCRIPTEN_BINDINGS(ezc3d_wasm) {
         .function("setOnes", &ezc3d::Matrix::setOnes)
         .function("setIdentity", &ezc3d::Matrix::setIdentity);
 
-    // --- Vector3d Bindings (Resolving Overloads) ---
+    // --- Vector3d Bindings (with Matrix inheritance) ---
     class_<ezc3d::Vector3d, base<ezc3d::Matrix>>("Vector3d")
         .constructor<>()
         .constructor<double, double, double>()
+        .constructor<const ezc3d::Matrix&>()
         .function("print", &ezc3d::Vector3d::print)
-        // Disambiguate Getters and Setters
         .function("x", select_overload<double() const>(&ezc3d::Vector3d::x))
         .function("set_x", select_overload<void(double)>(&ezc3d::Vector3d::x))
         .function("y", select_overload<double() const>(&ezc3d::Vector3d::y))
@@ -32,36 +33,24 @@ EMSCRIPTEN_BINDINGS(ezc3d_wasm) {
         .function("norm", &ezc3d::Vector3d::norm)
         .function("normalize", &ezc3d::Vector3d::normalize);
 
-    // --- Header Bindings (Resolving Overloads) ---
-    class_<ezc3d::Header>("Header")
-        .function("nbFrames", &ezc3d::Header::nbFrames)
-        .function("frameRate", select_overload<float() const>(&ezc3d::Header::frameRate))
-        .function("set_frameRate", select_overload<void(float)>(&ezc3d::Header::frameRate))
-        .function("nb3dPoints", select_overload<size_t() const>(&ezc3d::Header::nb3dPoints))
-        .function("nbAnalogs", select_overload<size_t() const>(&ezc3d::Header::nbAnalogs))
-        .function("firstFrame", select_overload<size_t() const>(&ezc3d::Header::firstFrame))
-        .function("lastFrame", select_overload<size_t() const>(&ezc3d::Header::lastFrame));
-
-    // --- Data Point Bindings ---
-    class_<ezc3d::DataNS::Points3dNS::Point>("Point")
-        .function("x", select_overload<double() const>(&ezc3d::DataNS::Points3dNS::Point::x))
-        .function("y", select_overload<double() const>(&ezc3d::DataNS::Points3dNS::Point::y))
-        .function("z", select_overload<double() const>(&ezc3d::DataNS::Points3dNS::Point::z))
-        .function("residual", select_overload<double() const>(&ezc3d::DataNS::Points3dNS::Point::residual));
-
-    // --- Main C3D Binding ---
-    class_<ezc3d::c3d>("c3d")
+    // --- Specialized Matrix Bindings ---
+    class_<ezc3d::Matrix33, base<ezc3d::Matrix>>("Matrix33")
         .constructor<>()
-        .constructor<std::string>()
-        .function("write", &ezc3d::c3d::write)
-        .function("header", &ezc3d::c3d::header)
-        .function("parameters", &ezc3d::c3d::parameters)
-        .function("data", &ezc3d::c3d::data)
-        .function("pointNames", &ezc3d::c3d::pointNames)
-        .function("channelNames", &ezc3d::c3d::channelNames);
+        .constructor<double, double, double, double, double, double, double, double, double>()
+        .constructor<const ezc3d::Matrix&>();
 
-    // --- Vector Utility Registrations ---
-    register_vector<std::string>("StringVector");
-    register_vector<ezc3d::Vector3d>("Vector3dVector");
-    register_vector<double>("DoubleVector");
-}
+    class_<ezc3d::Matrix44, base<ezc3d::Matrix>>("Matrix44")
+        .constructor<>()
+        .constructor<double, double, double, double, double, double, double, double, double, double, double, double, double, double, double, double>()
+        .constructor<const ezc3d::Matrix&>();
+
+    // --- Force Platform Bindings ---
+    class_<ezc3d::Modules::ForcePlatform>("ForcePlatform")
+        .function("nbFrames", &ezc3d::Modules::ForcePlatform::nbFrames)
+        .function("forceUnit", &ezc3d::Modules::ForcePlatform::forceUnit)
+        .function("momentUnit", &ezc3d::Modules::ForcePlatform::momentUnit)
+        .function("positionUnit", &ezc3d::Modules::ForcePlatform::positionUnit)
+        .function("calMatrix", &ezc3d::Modules::ForcePlatform::calMatrix)
+        .function("corners", &ezc3d::Modules::ForcePlatform::corners)
+        .function("origin", &ezc3d::Modules::ForcePlatform::origin)
+        .function("forces", &ezc3
